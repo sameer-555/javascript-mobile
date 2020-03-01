e0b20edda712e5edec800cf427460881ac97fe29
sameer-555 <sameerborkar6@gmail.com>
2020-03-01 13:11:56 +0530

    new features

--- test.js
+++ test.js
@@ -1,10 +1,33 @@
 function setup() {
     createCanvas(displayWidth, displayHeight);
     strokeWeight(10);
-    stroke(0);
+    stroke("black");
+    fill(255)
 }
+color = "black"
+$(document).ready(function () {
+    $(".red").click(function () {
+        color = "red"
+    })
+    $(".green").click(function () {
+        color = "green"
+    })
+    $(".yellow").click(function () {
+        color = "yellow"
+    })
+    $(".blue").click(function () {
+        color = "blue"
+    })
+});
 
 function touchMoved() {
     line(mouseX, mouseY, pmouseX, pmouseY);
+    stroke(color)
     return false;
 }
+
+$(document).ready(function () {
+    $(".reload").click(function () {
+        location.reload()
+    })
+});
